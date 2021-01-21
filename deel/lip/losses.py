fda0b82881f9db61ba35d7579f99449baaa619b8
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-01-21 10:35:17 +0100

    handling both 1,0 and 1,-1 labels in binary classification, removed true_values param

--- deel/lip/losses.py
+++ deel/lip/losses.py
@@ -12,7 +12,7 @@ from .utils import _deel_export
 
 
 @_deel_export
-def KR_loss(true_values=(1, -1)):
+def KR_loss():
     r"""
     Loss to estimate wasserstein-1 distance using Kantorovich-Rubinstein duality.
     The Kantorovich-Rubinstein duality is formulated as following:
@@ -23,10 +23,7 @@ def KR_loss(true_values=(1, -1)):
         \underset{\textbf{x}  \sim \nu}{\mathbb{E}} \left[f(\textbf{x} )\right]
 
 
-    Where mu and nu stands for the two distributions each matching one the `true_values` with it's labels (y_true).
-
-    Args:
-        true_values: tuple containing the two label for each predicted class.
+    Where mu and nu stands for the two distributions, the distribution where the label is 1 and the rest.
 
     Returns:
         Callable, the function to compute Wasserstein loss
@@ -36,8 +33,8 @@ def KR_loss(true_values=(1, -1)):
     @tf.function
     def KR_loss_fct(y_true, y_pred):
         # create two boolean masks each selecting one distribution
-        S0 = tf.equal(y_true, true_values[0])
-        S1 = tf.equal(y_true, true_values[1])
+        S0 = tf.equal(y_true, 1)
+        S1 = tf.not_equal(y_true, 1)
         # compute the KR dual representation
         return K.mean(tf.boolean_mask(y_pred, S0)) - K.mean(tf.boolean_mask(y_pred, S1))
 
@@ -45,28 +42,24 @@ def KR_loss(true_values=(1, -1)):
 
 
 @_deel_export
-def neg_KR_loss(true_values=(1, -1)):
+def neg_KR_loss():
     r"""
     Loss to compute the negative wasserstein-1 distance using Kantorovich-Rubinstein
     duality. This allows the maximisation of the term using conventional optimizer.
-
-    Args:
-        true_values: tuple containing the two label for each predicted class
 
     Returns:
         Callable, the function to compute negative Wasserstein loss
 
     """
-
     @tf.function
     def neg_KR_loss_fct(y_true, y_pred):
-        return -KR_loss(true_values)(y_true, y_pred)
+        return -KR_loss()(y_true, y_pred)
 
     return neg_KR_loss_fct
 
 
 @_deel_export
-def HKR_loss(alpha, min_margin=1, true_values=(1, -1)):
+def HKR_loss(alpha, min_margin=1):
     r"""
     Wasserstein loss with a regularization param based on hinge loss.
 
@@ -78,7 +71,6 @@ def HKR_loss(alpha, min_margin=1, true_values=(1, -1)):
     Args:
         alpha: regularization factor
         min_margin: minimal margin ( see hinge_margin_loss )
-        true_values: tuple containing the two label for each predicted class, used to compute the
         Kantorovich-rubinstein term of the loss. In order to be consistent between hinge and KR, the first label must
         yield the positve class while the second yields negative class.
 
@@ -94,9 +86,7 @@ def HKR_loss(alpha, min_margin=1, true_values=(1, -1)):
         else:
             # true value: positive value should be the first to be coherent with the
             # hinge loss (positive y_pred)
-            return alpha * hinge_margin_loss(min_margin)(y_true, y_pred) - KR_loss(
-                true_values
-            )(y_true, y_pred)
+            return alpha * hinge_margin_loss(min_margin)(y_true, y_pred) - KR_loss()(y_true, y_pred)
 
     return HKR_loss_fct
 
@@ -199,9 +189,9 @@ def HKR_multiclass_loss(alpha=0.0, min_margin=1):
 
     @tf.function
     def HKR_multiclass_loss_fct(y_true, y_pred):
-        if alpha == np.inf:  # alpha negative hinge only
+        if alpha == np.inf:  # alpha = inf => hinge only
             return hingeloss(y_true, y_pred)
-        elif alpha == 0.0:
+        elif alpha == 0.0:  # alpha = 0 => KR only
             return -KRloss(y_true, y_pred)
         else:
             return -KRloss(y_true, y_pred) / alpha + hingeloss(y_true, y_pred)
