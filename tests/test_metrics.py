9981711ee15bb64bc0650a19de15e4f6b136716e
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-11-23 10:42:49 +0100

    corrected tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -14,35 +14,48 @@ import numpy as np
 import os
 
 
-def check_serialization(nb_class, loss):
-    m = Sequential([Input(10), Dense(nb_class)])
+def check_serialization(nb_class, loss, nb_classes):
+    n = 255
+    x = tf.random.uniform((n, 42))
+    y = tf.one_hot(
+        tf.convert_to_tensor(np.random.randint(nb_classes, size=n)), nb_classes
+    )
+    m = Sequential([Input(42), Dense(nb_class)])
     m.compile(optimizer=SGD(), loss=loss)
+    l1 = m.evaluate(x, y)
     name = loss.__class__.__name__
     path = os.path.join("logs", "losses", name)
     m.save(path)
     m2 = load_model(path, compile=True)
-    m2(tf.random.uniform((255, 10)))
+    l2 = m2.evaluate(x, y)
+    return l1, l2
 
 
 class Test(TestCase):
     def test_serialization(self):
         pra = ProvableRobustAccuracy(1, 1.0, disjoint_neurons=False)
-        check_serialization(1, pra)
+        l1, l2 = check_serialization(10, pra, 10)
+        self.assertEqual(
+            l1, l2, "serialization changed loss value for ProvableRobustAccuracy"
+        )
         par = ProvableAvgRobustness(
             2.0, disjoint_neurons=False, negative_robustness=True
         )
-        check_serialization(1, par)
+        l1, l2 = check_serialization(10, par, 10)
+        self.assertEqual(
+            l1, l2, "serialization changed loss value for ProvableAvgRobustness"
+        )
 
     def test_provable_vs_adjusted(self):
         n = 500
-        x1 = tf.random.normal((n, 10), mean=0.0, stddev=0.1)
-        x2 = tf.random.normal((n, 10), mean=1.0, stddev=0.1)
+        x = tf.random.normal((n, 10), mean=0.0, stddev=0.1)
+        y = tf.one_hot(tf.convert_to_tensor(np.random.randint(10, size=n)), 10)
         for lip_cst in [1, 0.5, 2.0]:
             for disjoint in [True, False]:
                 pr = ProvableAvgRobustness(lip_cst, disjoint, negative_robustness=False)
                 ar = ProvableAvgRobustness(lip_cst, disjoint, negative_robustness=True)
-                l1 = pr(x1, x1).numpy()
-                l2 = ar(x1, x1).numpy()
+                l1 = pr(y, y).numpy()
+                l2 = ar(y, y).numpy()
                 self.assertAlmostEqual(
                     l1,
                     l2,
@@ -51,8 +64,8 @@ class Test(TestCase):
                     "robustness must give same "
                     "values when y_true==y_pred",
                 )
-                l1 = pr(x1, x2).numpy()
-                l2 = ar(x1, x2).numpy()
+                l1 = pr(y, x).numpy()
+                l2 = ar(y, x).numpy()
                 self.assertNotAlmostEqual(
                     l1,
                     l2,
@@ -61,13 +74,13 @@ class Test(TestCase):
                     " when y_true!=y_pred",
                 )
 
-        x1 = tf.random.normal((n, 10), mean=0.0, stddev=0.1)
-        x2 = tf.random.normal((n, 10), mean=1.0, stddev=0.1)
+        x = tf.random.normal((n, 10), mean=0.0, stddev=0.1)
+        y = tf.one_hot(tf.convert_to_tensor(np.random.randint(10, size=n)), 10)
         for lip_cst in [1.0, 0.5, 2.0]:
             bpr = ProvableAvgRobustness(lip_cst, disjoint, negative_robustness=False)
             bar = ProvableAvgRobustness(lip_cst, disjoint, negative_robustness=True)
-            l1 = bpr(x1, x1).numpy()
-            l2 = bar(x1, x1).numpy()
+            l1 = bpr(y, y).numpy()
+            l2 = bar(y, y).numpy()
             self.assertAlmostEqual(
                 l1,
                 l2,
@@ -75,8 +88,8 @@ class Test(TestCase):
                 msg="provable and adjusted robustness must give same values when y_"
                 "true==y_pred",
             )
-            l1 = bpr(x1, x2).numpy()
-            l2 = bar(x1, x2).numpy()
+            l1 = pr(y, x).numpy()
+            l2 = ar(y, x).numpy()
             self.assertNotAlmostEqual(
                 l1,
                 l2,
@@ -163,14 +176,11 @@ class Test(TestCase):
         for neg_val in [0.0, -1.0]:
             y_pred_case = y_pred
             y_true_case = tf.where(y_true > 0, 1.0, neg_val)
+            # no sqrt(2)/2 here as the corrective factor works only in multiclass
             metrics_values["standard"].append(pdr(y_true_case, y_pred_case).numpy())
-            metrics_values["standard"].append(
-                pr(y_true_case, y_pred_case * (np.sqrt(2) / 2.0)).numpy()
-            )
+            metrics_values["standard"].append(pr(y_true_case, y_pred_case).numpy())
             metrics_values["negative_rob"].append(adr(y_true_case, y_pred_case).numpy())
-            metrics_values["negative_rob"].append(
-                ar(y_true_case, y_pred_case * (np.sqrt(2) / 2.0)).numpy()
-            )
+            metrics_values["negative_rob"].append(ar(y_true_case, y_pred_case).numpy())
         self.assertTrue(
             all(
                 [m == metrics_values["standard"][0] for m in metrics_values["standard"]]
