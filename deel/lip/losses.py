bf1fb313d98a5a60496aa742c4f87e0fedfdd2dd
cofri <cofri@users.noreply.github.com>
2021-12-14 17:15:22 +0100

    feat(losses): improve docstrings for losses

--- deel/lip/losses.py
+++ deel/lip/losses.py
@@ -17,7 +17,7 @@ from tensorflow.keras.utils import register_keras_serializable
 class KR(Loss):
     def __init__(self, reduction=Reduction.AUTO, name="KR"):
         r"""
-        Loss to estimate wasserstein-1 distance using Kantorovich-Rubinstein duality.
+        Loss to estimate Wasserstein-1 distance using Kantorovich-Rubinstein duality.
         The Kantorovich-Rubinstein duality is formulated as following:
 
         .. math::
@@ -30,8 +30,11 @@ class KR(Loss):
         Where mu and nu stands for the two distributions, the distribution where the
         label is 1 and the rest.
 
-        Returns:
-            Callable, the function to compute Wasserstein loss
+        Note that `y_true` and `y_pred` must be of rank 2: (batch_size, 1).
+
+        Args:
+            reduction: passed to tf.keras.Loss constructor
+            name: passed to tf.keras.Loss constructor
 
         """
         super(KR, self).__init__(reduction=reduction, name=name)
@@ -55,7 +58,7 @@ class KR(Loss):
 class HKR(Loss):
     def __init__(self, alpha, min_margin=1.0, reduction=Reduction.AUTO, name="HKR"):
         r"""
-        Wasserstein loss with a regularization param based on hinge loss.
+        Wasserstein loss with a regularization parameter based on the hinge margin loss.
 
         .. math::
             \inf_{f \in Lip_1(\Omega)} \underset{\textbf{x} \sim P_-}{\mathbb{E}}
@@ -64,17 +67,16 @@ class HKR(Loss):
             \underset{\textbf{x}}{\mathbb{E}} \left(\text{min_margin}
             -Yf(\textbf{x})\right)_+
 
+        Note that `y_true` and `y_pred` must be of rank 2: (batch_size, 1).
+
         Args:
             alpha: regularization factor
             min_margin: minimal margin ( see hinge_margin_loss )
-            Kantorovich-rubinstein term of the loss. In order to be consistent
-            between hinge and KR, the first label must yield the positve class
-            while the second yields negative class.
+                Kantorovich-Rubinstein term of the loss. In order to be consistent
+                between hinge and KR, the first label must yield the positive class
+                while the second yields negative class.
             reduction: passed to tf.keras.Loss constructor
             name: passed to tf.keras.Loss constructor
-
-        Returns:
-             a function that compute the regularized Wasserstein loss
 
         """
         self.alpha = alpha
@@ -113,13 +115,12 @@ class HingeMargin(Loss):
             \underset{\textbf{x}}{\mathbb{E}} \left(\text{min_margin}
             -Yf(\textbf{x})\right)_+
 
+        Note that `y_true` and `y_pred` must be of rank 2: (batch_size, 1).
+
         Args:
-            min_margin: the minimal margin to enforce.
+            min_margin: positive float, margin to enforce.
             reduction: passed to tf.keras.Loss constructor
             name: passed to tf.keras.Loss constructor
-
-        Returns:
-            a function that compute the hinge loss.
 
         """
         self.min_margin = min_margin
@@ -144,18 +145,15 @@ class HingeMargin(Loss):
 class MulticlassKR(Loss):
     def __init__(self, reduction=Reduction.AUTO, name="MulticlassKR"):
         r"""
-        Loss to estimate average of W1 distance using Kantorovich-Rubinstein duality
-        over outputs. Note y_true should be one hot encoding (labels being 1s and 0s
-        ). In this multiclass setup thr KR term is computed for each class and then
-        averaged.
+        Loss to estimate average of Wasserstein-1 distance using Kantorovich-Rubinstein
+        duality over outputs. In this multiclass setup, the KR term is computed for each
+        class and then averaged.
+
+        Note that `y_true` and `y_pred` should be one-hot encoded.
 
         Args:
             reduction: passed to tf.keras.Loss constructor
             name: passed to tf.keras.Loss constructor
-
-        Returns:
-            Callable, the function to compute Wasserstein multiclass loss.
-            #Note y_true has to be one hot encoded
 
         """
         self.eps = 1e-7
@@ -181,21 +179,18 @@ class MulticlassHinge(Loss):
         self, min_margin=1.0, reduction=Reduction.AUTO, name="MulticlassHinge"
     ):
         """
-        Loss to estimate the Hinge loss in a multiclass setup. It compute the
-        elementwise hinge term. Note that this formulation differs from the one
-        commonly found in tensorflow/pytorch (with marximise the difference between
+        Loss to estimate the Hinge loss in a multiclass setup. It computes the
+        element-wise Hinge term. Note that this formulation differs from the one
+        commonly found in tensorflow/pytorch (which maximises the difference between
         the two largest logits). This formulation is consistent with the binary
-        classification loss used in a multiclass fashion. Note y_true should be one
-        hot encoded. labels in (1,0)
+        classification loss used in a multiclass fashion.
+
+        Note that `y_true` and `y_pred` should be one-hot encoded.
 
         Args:
             min_margin: positive float, margin to enforce.
             reduction: passed to tf.keras.Loss constructor
             name: passed to tf.keras.Loss constructor
-
-        Returns:
-            Callable, the function to compute multiclass Hinge loss
-            #Note y_true has to be one hot encoded
 
         """
         self.min_margin = min_margin
@@ -235,15 +230,14 @@ class MulticlassHKR(Loss):
         The multiclass version of HKR. This is done by computing the HKR term over each
         class and averaging the results.
 
+        Note that `y_true` and `y_pred` should be one-hot encoded.
+
         Args:
             alpha: regularization factor
-            min_margin: minimal margin ( see Hinge_multiclass_loss )
+            min_margin: positive float, margin to enforce.
             reduction: passed to tf.keras.Loss constructor
             name: passed to tf.keras.Loss constructor
 
-        Returns:
-            Callable, the function to compute HKR loss
-            #Note y_true has to be one hot encoded
         """
         self.alpha = alpha
         self.min_margin = min_margin
@@ -273,21 +267,21 @@ class MulticlassHKR(Loss):
 
 @register_keras_serializable("deel-lip", "MultiMargin")
 class MultiMargin(Loss):
-    def __init__(self, min_margin=1, reduction=Reduction.AUTO, name="MultiMargin"):
+    def __init__(self, min_margin=1.0, reduction=Reduction.AUTO, name="MultiMargin"):
         """
-        Compute the mean hinge margin loss for multi class (equivalent to Pytorch
-         multi_margin_loss)
+        Compute the hinge margin loss for multiclass (equivalent to Pytorch
+        multi_margin_loss)
+
+        Note that `y_true` and `y_pred` should be one-hot encoded.
 
         Args:
-            min_margin: the minimal margin to enforce.
+            min_margin: positive float, margin to enforce.
             reduction: passed to tf.keras.Loss constructor
             name: passed to tf.keras.Loss constructor
 
         Notes:
             y_true has to be to_categorical
 
-        Returns:
-            Callable, the function to compute multi margin loss
         """
         self.min_margin = min_margin
         super(MultiMargin, self).__init__(reduction=reduction, name=name)
