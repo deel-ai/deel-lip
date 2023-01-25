bb83afd9f62f178ad945f238d05bdb27d390214b
cofri <cofri@users.noreply.github.com>
2023-01-25 17:14:25 +0100

    refactor: create layers/__init__.py
    New file `layers/__init__.py` to import all layers with the same API as before:
    `import deel.lip.layers.SpectralConv2D`
    Only activations have a new API:
    `import deel.lip.layers.GroupSort2`
    and unconstrained layers:
    `import deel.lip.layers.unconstrained.PadConv2D`

---
+++ deel/lip/layers/__init__.py
@@ -0,0 +1,13 @@
+from . import unconstrained
+from .activations import FullSort, GroupSort, GroupSort2, Householder, MaxMin, PReLUlip
+from .base_layer import Condensable, LipschitzLayer
+from .convolutional import FrobeniusConv2D, SpectralConv2D, SpectralConv2DTranspose
+from .dense import FrobeniusDense, SpectralDense
+from .pooling import (
+    InvertibleDownSampling,
+    InvertibleUpSampling,
+    ScaledAveragePooling2D,
+    ScaledGlobalAveragePooling2D,
+    ScaledGlobalL2NormPooling2D,
+    ScaledL2NormPooling2D,
+)
